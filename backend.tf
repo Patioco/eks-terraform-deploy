9a82bc287e0d4f6489b23ab1359cdee639ae6b9e
Patioco <116298890+Patioco@users.noreply.github.com>
2024-05-04 09:54:35 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,27 @@
+resource "aws_s3_bucket" "mybucket"{
+    bucket = "patosoft-terraform-backend-bucket"
+    versioning {
+        enabled = true
+    }
+    server_side_encryption_configuration {
+        rule {
+          apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+          }
+      }
+    }
+}
+resource "aws_dynamodb_table" "terraform-state-locking" {
+    name = "terraform-state-locking"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
+
 terraform {
   backend "s3" {
     bucket = "patosoft-terraform-backend-bucket"

24f18bbb3fdd02f63a4150e54bcb6fcd5e4fb637
Patioco <116298890+Patioco@users.noreply.github.com>
2024-05-04 08:10:25 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "patosoft-terraform-backend-bucket"
     key    = "infra_resources/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
 }
